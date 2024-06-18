af1ad4608a3655fab864ef215e50701cf3c03bbc
Szövérdffy György <szoverdffy.gyorgy@gmail.com>
2024-06-18 14:43:25 +0200

    #13: Created /vehicles/:id endpoint. Vehicle position update possible.

--- src/backend/src/main/java/com/example/springboot/controller/VehicleController.java
+++ src/backend/src/main/java/com/example/springboot/controller/VehicleController.java
@@ -1,12 +1,14 @@
 package com.example.springboot.controller;
 
 import com.example.springboot.dto.RegisterVehicleResponseDto;
+import com.example.springboot.dto.UpdateVehiclePositionDto;
+import com.example.springboot.exceptions.VehicleNotFoundException;
 import com.example.springboot.model.Vehicle;
 import com.example.springboot.service.VehicleService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
 
 @RestController
 @RequestMapping("/vehicles")
@@ -19,9 +21,19 @@ public class VehicleController {
     }
 
     @PostMapping
+    @ResponseBody
     public RegisterVehicleResponseDto registerVehicle() {
         Vehicle registeredVehicle = this.vehicleService.registerVehicle();
 
         return new RegisterVehicleResponseDto(registeredVehicle.getId());
+    }
+
+    @PutMapping("/{vehicleId}")
+    public void updateVehiclePosition(@RequestBody UpdateVehiclePositionDto position, @PathVariable Long vehicleId) {
+        try {
+            this.vehicleService.updateVehiclePosition(vehicleId, position.getLatitude(), position.getLongitude());
+        } catch (VehicleNotFoundException e) {
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, e.getMessage(), e);
+        }
     }
 }
---
+++ src/backend/src/main/java/com/example/springboot/dto/UpdateVehiclePositionDto.java
@@ -0,0 +1,22 @@
+package com.example.springboot.dto;
+
+public class UpdateVehiclePositionDto {
+    private float latitude;
+    private float longitude;
+
+    public float getLongitude() {
+        return longitude;
+    }
+
+    public void setLongitude(float longitude) {
+        this.longitude = longitude;
+    }
+
+    public float getLatitude() {
+        return latitude;
+    }
+
+    public void setLatitude(float latitude) {
+        this.latitude = latitude;
+    }
+}
