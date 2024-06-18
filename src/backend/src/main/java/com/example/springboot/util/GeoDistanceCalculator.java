a3b79d0d26b2acf9c04a5342e975351181ed8e96
Szövérdffy György <szoverdffy.gyorgy@gmail.com>
2024-06-18 23:08:42 +0200

    #11: Added line break.

--- src/backend/src/main/java/com/example/springboot/util/GeoDistanceCalculator.java
+++ src/backend/src/main/java/com/example/springboot/util/GeoDistanceCalculator.java
@@ -15,6 +15,7 @@ public class GeoDistanceCalculator {
 
         double a = Math.sin(latDelta / 2) * Math.sin(latDelta / 2) + Math.cos(lat1) * Math.cos(lat2) * Math.sin(lonDelta / 2) * Math.sin(lonDelta / 2);
         double c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
+
         return EARTH_RADIUS_IN_METRES * c;
     }
 }
