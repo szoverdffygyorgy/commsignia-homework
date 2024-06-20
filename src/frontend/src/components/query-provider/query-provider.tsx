d89142d0bd2602ae95df734df8dc196f6233fa48
Szövérdffy György <szoverdffy.gyorgy@gmail.com>
2024-06-20 14:44:48 +0200

    #3: react-query added with axios.

---
+++ src/frontend/src/components/query-provider/query-provider.tsx
@@ -0,0 +1,12 @@
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { PropsWithChildren } from "react";
+
+const QueryProvider = ({ children }: PropsWithChildren) => {
+  const queryClient = new QueryClient();
+
+  return (
+    <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
+  );
+};
+
+export default QueryProvider;
