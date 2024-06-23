e909ad2c27038f29a4df51d7a35983744332a5f2
Szövérdffy György <szoverdffy.gyorgy@gmail.com>
2024-06-23 16:44:02 +0200

    #48: React query refreshes every 5 seconds.

--- src/frontend/src/components/query-provider/query-provider.tsx
+++ src/frontend/src/components/query-provider/query-provider.tsx
@@ -2,7 +2,9 @@ import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { PropsWithChildren } from "react";
 
 const QueryProvider = ({ children }: PropsWithChildren) => {
-  const queryClient = new QueryClient();
+  const queryClient = new QueryClient({
+    defaultOptions: { queries: { refetchInterval: 5000 } },
+  });
 
   return (
     <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
